0f69980c8059fe8a4dafbfed7fa85483b2c4501e
Jean Sousa <jeansousa@iftm.edu.br>
2021-08-11 20:13:03 -0300

    Primeira aula

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::resources([
+	"cliente" => ClienteController::class
+]);
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('teste.welcome');
 });
